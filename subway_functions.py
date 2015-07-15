9b16b2267becf2c22c96a66cd5e09550200c22cc
Reuven Lazarus <rzl@google.com>
2015-07-15 17:39:11 -0400

    Edits to the string interpolations lab
    - Changed a lot of Ruby-centric wording to Python, like puts->print and
      method->function.
    - Renamed subway_methods.py to subway_functions.py accordingly.
    - Pointed out that calling map_size doesn't print its output, so that
      students can find the bug and fix it.
    - Removed the dice rolling exercise; in Python that requires importing a
      library, which we get to later.

---
+++ subway_functions.py
@@ -0,0 +1,63 @@
+# A subway story
+
+# You hop on the subway at Union Square. As you are waiting for the train you
+# take a look at the subway map. The map is about 21 inches wide and 35 inches
+# tall. Let's write a function to return the area of the map:
+def map_size(width, height):
+    map_area = width * height
+    return "The map is %d square inches" % (map_area)
+
+# Now you give it a shot! It takes about 156 seconds to go between stops and
+# you'll be taking the train for 3 stops. Write a function that calculates how
+# long your trip will take.
+def trip_length(...):
+  #  put your code here
+
+
+# The train arrives and you hop on. Guess what time it is? It's showtime! There
+# are 23 people on the train and each person gives the dancers 1.5 dollars.
+# Write a function that returns how much money they made.
+
+
+
+# There is one grumpy lady on the train that doesn't like the dancing though.
+# Write a function called stop_dancing that returns a message to the dancers in
+# all caps.
+
+
+
+
+# There is also a really enthusiastic rider who keeps shouting "Everything is
+# awesome!" Write a function that returns everything is awesome 5 times.
+
+
+
+
+# You are almost at your stop and you start thinking about how you are going to
+# get home. You have $18 left on your metro card. Write a function that returns
+# how many trips you have left.
+
+
+
+
+
+# Call your functions below:
+print "How big is that subway map?"
+# Call your function here - like this:
+map_size(21, 35)
+# ... but that doesn't output anything. Hmm. See if you can fix it.
+
+print "This is how long the trip will take"
+trip_length(...)
+
+print "How much money did the train dancers make?"
+# call your function here
+
+print "That lady told the train dancers to"
+# call your function here
+
+print "That guy kept shouting"
+# call your function here
+
+print "This is how many trips I have left on my metrocard"
+# call your function here
